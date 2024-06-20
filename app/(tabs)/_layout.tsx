52badd462295d02e68e6a4744c0ad2107c69f55b
Francisco M von Gerstenberg <fmvong@gmail.com>
2024-06-20 16:45:25 -0300

    tab style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,34 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import {CalendarIcon} from "react-native-heroicons/outline";
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from "expo-router";
+import React from "react";
+import { CalendarIcon } from "react-native-heroicons/outline";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "#00B47D",
         headerShown: false,
-      }}>
+        tabBarStyle: {
+          backgroundColor: "#FFFFFF",
+          elevation: 8,
+          shadowColor: "#000000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          gap: 4,
+          paddingBottom: 20,
+          paddingTop: 20,
+          height: 80,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Calendar',
+          title: "Calendar",
           tabBarIcon: ({ color, focused }) => (
-            <CalendarIcon color={color} /> 
+            <CalendarIcon color={"#00B47D"} />
           ),
         }}
       />
